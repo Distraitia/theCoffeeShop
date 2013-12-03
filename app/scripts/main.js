0b94fbc78a1ce850db18753d6c9b25504b4a7f21
Allison Lewis <alea218@gmail.com>
2013-12-02 19:05:25 -0500

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+console.log('\'Allo \'Allo!');
+
+console.log("Let's do this!");
+
+angular.module('coffeeHouseApp', ['ngRoute', 'ngAnimate'])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/home.html',
+				controller: 'homeCtrl'
+			})
+		})
+	
+
+var app = angular.module('coffeeHouseApp');
+
+app.controller('HomeCtrl', function() {
+	console.log('HomeCtrl');
+})
